0957a6afdf1697013e258fdb25990220aa1e1e18
JinJeon <nanyong01@gmail.com>
2022-06-15 01:20:26 +0900

    refactor: option tabs의 반복 부분 리펙토링

--- fe/src/Component/OptionsBox/OptionTabs/index.tsx
+++ fe/src/Component/OptionsBox/OptionTabs/index.tsx
@@ -7,22 +7,35 @@ const MILESTONE = "마일스톤";
 const ADD_ISSUE = "이슈 작성";
 const labelCount = 2;
 const milestoneCount = 3;
+const tabsInfo = [
+	{
+		id: 1,
+		Icon: BookmarksOutlined,
+		name: LABEL,
+		count: labelCount,
+	},
+	{
+		id: 2,
+		Icon: DirectionsOutlined,
+		name: MILESTONE,
+		count: milestoneCount,
+	},
+];
 
 const OptionTabs = () => {
+	const tabs = tabsInfo.map(({ id, Icon, name, count }) => {
+		return (
+			<div key={id}>
+				<Icon colorset="label" size={20} />
+				<div>{name}</div>
+				<div>({count})</div>
+			</div>
+		);
+	});
+
 	return (
 		<StyledOptionTabs>
-			<StyledTabsLabelMilestone>
-				<div>
-					<BookmarksOutlined colorset="label" size={20} />
-					<div>{LABEL}</div>
-					<div>({labelCount})</div>
-				</div>
-				<div>
-					<DirectionsOutlined colorset="label" size={20} />
-					<div>{MILESTONE}</div>
-					<div>({milestoneCount})</div>
-				</div>
-			</StyledTabsLabelMilestone>
+			<StyledTabsLabelMilestone>{tabs}</StyledTabsLabelMilestone>
 			<StyledAddIssue>
 				<AddBox colorset="offWhite" size={20} />
 				{ADD_ISSUE}
