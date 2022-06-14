9505ff41b1738af7c9af87eca9561414a4d259e1
dukcode <duk9741@gmail.com>
2022-06-15 01:43:12 +0900

    docs: Swagger 동작하도록 환경 설정

---
+++ BE/src/main/java/com/team31/codesquad/issuetracker/config/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.team31.codesquad.issuetracker.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableWebMvc
+@Configuration
+public class SwaggerConfig {
+
+    private static final String API_NAME = "codesquad_issuetracker_team31";
+    private static final String API_VERSION = "1.0.0";
+    private static final String API_DESCRIPTION = "CodeSquad Issue Tracker API 명세서";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.team31.codesquad.issuetracker.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+
+}
