ebced509f255c3cff4c9e1ee07f18e6c3e4a521a
dukcode <duk9741@gmail.com>
2022-06-15 21:43:42 +0900

    feat: LoginId ArgumentResolver 적용

---
+++ BE/src/main/java/com/team31/codesquad/issuetracker/config/mvc/WebConfig.java
@@ -0,0 +1,19 @@
+package com.team31.codesquad.issuetracker.config.mvc;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LogInIdArgumentResolver logInIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(logInIdArgumentResolver);
+    }
+}
