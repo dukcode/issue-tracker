bc51657d12c72e4e734d6c9ae2e42cc5aaee9985
dukcode <duk9741@gmail.com>
2022-06-16 00:16:50 +0900

    fix: 토큰이 존재하지 않는 경우 예외 발생

--- BE/src/main/java/com/team31/codesquad/issuetracker/web/interceptor/JwtInterceptor.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/web/interceptor/JwtInterceptor.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -16,7 +17,11 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
             Object handler) throws Exception {
-        String token = request.getHeader("Authorization").substring(7);
+        String header = request.getHeader("Authorization");
+        if (!StringUtils.hasText(header)) {
+            throw new PermissionDeniedException("토큰이 존재하지 않습니다.");
+        }
+        String token = header.substring(7);
         if (!jwtUtil.validateToken(token)) {
             throw new PermissionDeniedException("유효하지 않은 토큰입니다.");
         }
