f4ae0d122a3506806b69597eeb2f522fa6f46cb2
JinJeon <nanyong01@gmail.com>
2022-06-16 17:17:18 +0900

    feat: cookie 생성 및 임시 적용

--- fe/src/Component/Loading/index.tsx
+++ fe/src/Component/Loading/index.tsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+// import { useCookies } from "react-cookie";
 import axios from "axios";
 
 import { StyledLoading, StyledLoadingMention, StyledLoadingAnimation } from "./Loading.styled";
 
 const LOADING = "LOADING";
+// const USER_INFO = "userInfo";
 
 const Loading = () => {
+	// const [cookies, setCookie] = useCookies([USER_INFO]);
+	// console.log(cookies);
 	const { search } = useLocation();
 	const navigate = useNavigate();
 	const baseURL = `${process.env.REACT_APP_LOGIN_SERVER}${search}`;
 	const client = axios.create({
 		baseURL,
 		headers: {
-			"Content-Type": "text/plain",
+			"Content-Type": "application/x-www-form-urlencoded",
 		},
 		withCredentials: true,
 	});
@@ -22,6 +26,12 @@ const Loading = () => {
 		try {
 			const response = await client.get("/", { withCredentials: true });
 			console.log(response);
+			// const { profileImage, accessToken } = response;
+			// setCookie(
+			// 	USER_INFO,
+			// 	{ profileImage, accessToken },
+			// 	{ path: "/", expires: new Date(2040, 11, 11) }
+			// );
 			navigate("/");
 		} catch (error) {
 			console.error(error);
