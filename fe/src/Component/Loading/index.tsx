4f5404c29f775ab54168529082388177d05f9136
JinJeon <nanyong01@gmail.com>
2022-06-16 16:15:20 +0900

    feat: github 로그인 후 data를 받는 부분 구현

--- fe/src/Component/Loading/index.tsx
+++ fe/src/Component/Loading/index.tsx
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+
 import { StyledLoading, StyledLoadingMention, StyledLoadingAnimation } from "./Loading.styled";
 
 const LOADING = "LOADING";
 
 const Loading = () => {
+	const { search } = useLocation();
 	const navigate = useNavigate();
+	const baseURL = `${process.env.REACT_APP_LOGIN_SERVER}${search}`;
+	const client = axios.create({
+		baseURL,
+		headers: {
+			"Content-Type": "text/plain",
+		},
+		withCredentials: true,
+	});
+
 	const getToken = async () => {
 		try {
-			const response = await fetch(
-				"https://github.com/login/oauth/authorize?client_id=a11a925a68811b821ef0"
-			);
-			const data = await response.json();
-			console.log(data);
-			navigate("/new-issue");
+			const response = await client.get("/", { withCredentials: true });
+			console.log(response);
+			navigate("/");
 		} catch (error) {
 			console.error(error);
+			// navigate("/login");
 		}
 	};
 
--- fe/src/Component/Login/index.tsx
+++ fe/src/Component/Login/index.tsx
@@ -1,5 +1,5 @@
 import icons from "Util/Icons";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import warning from "Img/warning.png";
 import {
 	StyledLogin,
@@ -13,19 +13,20 @@ const ISSUE_TRACKER = "ISSUE TRACKER";
 
 const Login = () => {
 	const { GitHub } = icons;
+	const loginUrl = process.env.REACT_APP_GITHUB_LOGIN;
 
 	return (
 		<StyledLogin>
 			<img src={warning} alt="logo" width={250} />
 			<StyledLoginTitle>{ISSUE_TRACKER}</StyledLoginTitle>
-			<Link to="/loading">
+			<a href={loginUrl}>
 				<StyledLoginButton>
 					<StyledLoginMention>
 						<GitHub colorset="white" size={18} />
 						{GITHUB_LOGIN}
 					</StyledLoginMention>
 				</StyledLoginButton>
-			</Link>
+			</a>
 		</StyledLogin>
 	);
 };
