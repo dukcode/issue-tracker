27715da71e0f070a33463cea42e5079932504f95
dukcode <duk9741@gmail.com>
2022-06-16 17:53:29 +0900

    fix: CORS 문제 해결

--- BE/src/main/java/com/team31/codesquad/issuetracker/config/mvc/WebConfig.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/config/mvc/WebConfig.java
@@ -2,8 +2,10 @@ package com.team31.codesquad.issuetracker.config.mvc;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginNameArgumentResolver);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        }
     }
 }
