0eaa89c7ab3d785c9949bfab12bd93fbcfb924ee
dukcode <duk9741@gmail.com>
2022-06-19 04:03:49 +0900

    docs: Swagger 명세 보강

--- BE/src/main/java/com/team31/codesquad/issuetracker/web/controller/LabelController.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/web/controller/LabelController.java
@@ -5,6 +5,8 @@ import com.team31.codesquad.issuetracker.dto.label.LabelCreateRequest;
 import com.team31.codesquad.issuetracker.dto.label.LabelResponse;
 import com.team31.codesquad.issuetracker.dto.label.LabelUpdateRequest;
 import com.team31.codesquad.issuetracker.service.LabelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,29 +20,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Label", description = "Label 관련 API")
 @RequiredArgsConstructor
 @RestController
 public class LabelController {
 
     private final LabelService labelService;
 
+    @Tag(name = "Label")
+    @Operation(summary = "Label 목록 조회",
+            description = "Label의 목록을 조회한다.")
     @GetMapping("/api/v1/labels")
     public CountResult<List<LabelResponse>> getLabels() {
         return labelService.findAll();
     }
 
+    @Tag(name = "Label")
+    @Operation(summary = "Label 등록",
+            description = "Label을 등록한다.")
     @PostMapping("/api/v1/labels")
     public ResponseEntity<Long> createLabel(@Validated @RequestBody LabelCreateRequest request) {
         Long createdLabelId = labelService.createLabel(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdLabelId);
     }
 
+    @Tag(name = "Label")
+    @Operation(summary = "Label 삭제",
+            description = "Label을 삭제한다.")
     @DeleteMapping("/api/v1/labels/{labelId}")
     public ResponseEntity<Void> deleteLabel(@PathVariable Long labelId) {
         labelService.deleteLabel(labelId);
         return ResponseEntity.noContent().build();
     }
 
+    @Tag(name = "Label")
+    @Operation(summary = "Label 수정",
+            description = "Label을 수정한다.")
     @PutMapping("/api/v1/labels/{labelId}")
     public ResponseEntity<Void> updateLabel(
             @PathVariable Long labelId,
@@ -49,6 +64,9 @@ public class LabelController {
         return ResponseEntity.noContent().build();
     }
 
+    @Tag(name = "Label")
+    @Operation(summary = "Label 갯수 조회",
+            description = "Label의 전체 갯수를 조회한다.")
     @GetMapping("/api/v1/labels/count")
     public Long getCount() {
         return labelService.getCount();
