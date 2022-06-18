3c6bcf48e04ba6d968f5540f7d243dacdc62406f
dukcode <duk9741@gmail.com>
2022-06-19 00:41:48 +0900

    feat: Comment 등록, 수정, 삭제 API 구현

--- BE/src/main/java/com/team31/codesquad/issuetracker/domain/comment/Reaction.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/domain/comment/Reaction.java
@@ -27,11 +27,11 @@ public class Reaction extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "comment_id", nullable = false)
+    @JoinColumn(name = "comment_id")
     private Comment comment;
 
     @Enumerated(EnumType.STRING)
--- BE/src/main/java/com/team31/codesquad/issuetracker/web/controller/CommentController.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/web/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.team31.codesquad.issuetracker.web.controller;
 
 import com.team31.codesquad.issuetracker.config.mvc.annotation.LoginName;
 import com.team31.codesquad.issuetracker.dto.comment.CommentCreateRequest;
+import com.team31.codesquad.issuetracker.dto.comment.CommentUpdateRequest;
 import com.team31.codesquad.issuetracker.dto.reaction.ReactionCreateRequest;
 import com.team31.codesquad.issuetracker.service.CommentService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +24,25 @@ public class CommentController {
 
     @PostMapping("/api/v1/issues/{issueId}/comments")
     public ResponseEntity<Long> createComment(@PathVariable Long issueId,
-            @Validated @RequestBody CommentCreateRequest request) {
-        Long createdCommentId = commentService.createComment(issueId, request);
+            @Validated @RequestBody CommentCreateRequest request,
+            @LoginName String loginName) {
+        Long createdCommentId = commentService.createComment(issueId, request, loginName);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCommentId);
+    }
+
+    @DeleteMapping("/api/v1/issues/{issueId}/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long issueId,
+            @PathVariable Long commentId, @LoginName String loginName) {
+        commentService.deleteComment(issueId, commentId, loginName);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/api/v1/issues/{issueId}/comments/{commentId}")
+    public ResponseEntity<Void> updateComment(@PathVariable Long issueId,
+            @PathVariable Long commentId, @RequestBody CommentUpdateRequest request,
+            @LoginName String loginName) {
+        commentService.update(issueId, commentId, request, loginName);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/api/v1/issues/{issueId}/comments/{commentId}/reactions")
