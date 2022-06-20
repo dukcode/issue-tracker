175c412fa9c3365147af78a93cf73a67e3684767
dukcode <duk9741@gmail.com>
2022-06-20 17:07:33 +0900

    fix: CORS 문제 해결

--- BE/src/main/java/com/team31/codesquad/issuetracker/config/mvc/WebConfig.java
+++ BE/src/main/java/com/team31/codesquad/issuetracker/config/mvc/WebConfig.java
@@ -25,6 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("POST", "OPTIONS", "GET", "DELETE", "PUT", "POST", "PATCH")
                         .exposedHeaders("Authorization")
                         .allowCredentials(false).maxAge(3600);
             }
