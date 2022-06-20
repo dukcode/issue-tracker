1f3ef8740da7bb9484ae244d737cec0e10766ca7
JinJeon <nanyong01@gmail.com>
2022-06-20 14:22:04 +0900

    feat: 서버 내 토큰 확인 후 로그인 여부 판단하도록 설정

--- fe/src/Pages/Main/index.tsx
+++ fe/src/Pages/Main/index.tsx
@@ -1,14 +1,49 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import axios, { AxiosError } from "axios";
 import Header from "Pages/Main/Header";
 import StyledMain from "./Main.styled";
 
 const USER_INFO = "userInfo";
 
+const Loading = () => {
+	return <div>로딩입니다</div>;
+};
+
 const Main = () => {
+	const [outlet, setOutlet] = useState(<Loading />);
 	const [cookies] = useCookies([USER_INFO]);
-	const hasCookie = !(Object.keys(cookies).length === 0 && cookies.constructor === Object);
-	const outlet = hasCookie ? <Outlet /> : <Navigate to="login" />;
+	const navigate = useNavigate();
+
+	const checkCookies = async () => {
+		const hasCookie = !(Object.keys(cookies).length === 0);
+		if (!hasCookie) navigate("/login");
+
+		// const token = cookies.userInfo.accessToken;
+		const token = "dukcode";
+		const baseURL = `${process.env.REACT_APP_API}`;
+		const client = axios.create({
+			baseURL,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		try {
+			await client.get("/labels");
+			setOutlet(<Outlet />);
+		} catch (error) {
+			const e = error as AxiosError;
+			console.error(e); // eslint-disable-line no-console
+			navigate("/login");
+		}
+	};
+
+	useEffect(() => {
+		checkCookies();
+	}, []);
 
 	return (
 		<StyledMain>
