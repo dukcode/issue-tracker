54e5da6431bdab1967bae62d4c0975f75e8f11f1
Serin-Kim <serin9864@gmail.com>
2022-06-21 20:51:56 +0900

    feat: issueListApi 적용
    Co-authored-by: JinJeon <JinJeon@users.noreply.github.com>

---
+++ fe/src/Api/issueListApi.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const baseURL = `${process.env.REACT_APP_API}/issues`;
+const client = axios.create({ baseURL });
+
+const issueListApi = {
+	getIssueList: async (token: string) => {
+		const response = await client
+			.get("", {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.catch((error: Error | AxiosError) => {
+				if (axios.isAxiosError(error)) return error.response as AxiosResponse;
+				return { data: error, status: null };
+			});
+
+		return response;
+	},
+};
+
+export default issueListApi;
--- fe/src/Pages/IssueList/index.tsx
+++ fe/src/Pages/IssueList/index.tsx
@@ -1,5 +1,10 @@
-import icons from "Util/Icons";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Checkbox from "@mui/material/Checkbox";
+
+import issueListApi from "Api/issueListApi";
+import useCookieUserInfo from "Hooks";
+import icons from "Util/Icons";
 import OptionsBox from "Component/OptionsBox";
 import {
 	IssueContainer,
@@ -10,18 +15,9 @@ import {
 	ClosedIssue,
 	StyledCheckbox,
 } from "./Home.styled";
-import FilterCategoryList from "./FilterCategoryList";
+import FilterCategoryList, { listItem } from "./FilterCategoryList";
 import IssueCell from "./IssueCell";
-
-import type { listItem } from "./FilterCategoryList";
-
-type TIssueItem = {
-	id: number;
-	title: string;
-	author: string;
-	timeStamp: string; // TODO: timestamp 형식
-	mileStone: string;
-};
+import TIssueData from "./mockData";
 
 const filterCategoryItems: listItem[] = [
 	{ id: 1, category: "담당자" },
@@ -31,48 +27,45 @@ const filterCategoryItems: listItem[] = [
 ];
 
 const { ErrorOutline, Inventory } = icons;
-
-// TODO: label 추가해야 함
-const issueCellItems: TIssueItem[] = [
-	{
-		id: 1,
-		title: "FE 이슈트래커 개발",
-		author: "Maeve",
-		timeStamp: "22 일",
-		mileStone: "[FE] issue-tracker Week 1",
-	},
-	{
-		id: 2,
-		title: "feat: Header 구현",
-		author: "Jinjeon",
-		timeStamp: "2 일",
-		mileStone: "[FE] issue-tracker Week 2",
-	},
-	{
-		id: 3,
-		title: "API 설계 및 DB 설계 문서화",
-		author: "dukcode",
-		timeStamp: "1 시간",
-		mileStone: "[BE] issue-tracker Week 1",
-	},
-];
+const OPENED_ISSUE = "열린 이슈";
+const CLOSED_ISSUE = "닫힌 이슈";
 
 const Home = () => {
-	const OPENED_ISSUE = "열린 이슈";
-	const CLOSED_ISSUE = "닫힌 이슈";
-	const issueCells = issueCellItems
-		.slice(0)
-		.reverse()
-		.map((item: TIssueItem) => (
-			<IssueCell
-				key={item.id}
-				id={item.id}
-				title={item.title}
-				author={item.author}
-				timeStamp={item.timeStamp}
-				mileStone={item.mileStone}
-			/>
-		));
+	const [issueCells, setIssueCells] = useState([]);
+	const navigate = useNavigate();
+	const cookieUserInfo = useCookieUserInfo();
+
+	const getIssueList = async () => {
+		const { accessToken } = cookieUserInfo;
+
+		if (!accessToken) {
+			navigate("/login");
+			return;
+		}
+
+		const issueListResponse = await issueListApi.getIssueList(accessToken);
+		const {
+			data: { data },
+		} = issueListResponse;
+
+		const newIssueCells = data
+			.reverse()
+			.map((item: TIssueData) => (
+				<IssueCell
+					key={item.id}
+					id={item.id}
+					title={item.title}
+					author={item.author.loginName}
+					timeStamp={item.createDate}
+					mileStone={item.milestone.title}
+				/>
+			));
+		setIssueCells(newIssueCells);
+	};
+
+	useEffect(() => {
+		getIssueList();
+	}, []);
 
 	return (
 		<>
