e6c2e64dfced41f4a221bce70e762b00b109bb70
JinJeon <nanyong01@gmail.com>
2022-06-23 13:54:51 +0900

    feat: user img component 색상만 담을 수 있도록 설정 변경 및 적용

--- fe/src/Component/FilterBar/index.tsx
+++ fe/src/Component/FilterBar/index.tsx
@@ -1,13 +1,21 @@
-import Popup from "Component/Popup";
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
+import Popup, { TPopupContentProps } from "Component/Popup";
 import icons from "Util/Icons";
 import { StyledFilterBar, StyledFilterSelector, StyledFilterInputArea } from "./FilterBar.styled";
 
 const FILTER = "필터";
 const { KeyboardArrowDown, Search } = icons;
 const defaultInputValue = "is:open";
+const popupTitle = "이슈 필터";
+const popupContents: TPopupContentProps[] = [
+	{ id: 1, name: "열린 이슈", image: "#FFFFFF", imageType: "color" },
+	{ id: 2, name: "내가 작성한 이슈" },
+	{ id: 3, name: "나에게 할당된 이슈" },
+	{ id: 4, name: "내가 댓글을 남긴 이슈" },
+	{ id: 5, name: "닫힌 이슈" },
+];
 
 const FilterBar = () => {
 	const [inputValue, setInputValue] = useState(defaultInputValue);
@@ -33,7 +41,7 @@ const FilterBar = () => {
 
 	return (
 		<StyledFilterBar>
-			<Popup isLeft={true}>
+			<Popup isLeft={true} title={popupTitle} contents={popupContents}>
 				<StyledFilterSelector>
 					<div>{FILTER}</div>
 					<KeyboardArrowDown colorset="label" size={20} />
