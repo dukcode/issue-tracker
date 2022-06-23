cc79f86243151eef92b5607b49a8ecb3e0ee7470
JinJeon <nanyong01@gmail.com>
2022-06-23 14:15:27 +0900

    fix: 테스트 작성 부분 삭제

--- fe/src/Component/FilterBar/index.tsx
+++ fe/src/Component/FilterBar/index.tsx
@@ -10,7 +10,7 @@ const { KeyboardArrowDown, Search } = icons;
 const defaultInputValue = "is:open";
 const popupTitle = "이슈 필터";
 const popupContents: TPopupContentProps[] = [
-	{ id: 1, name: "열린 이슈", image: "#FFFFFF", imageType: "color" },
+	{ id: 1, name: "열린 이슈" },
 	{ id: 2, name: "내가 작성한 이슈" },
 	{ id: 3, name: "나에게 할당된 이슈" },
 	{ id: 4, name: "내가 댓글을 남긴 이슈" },
