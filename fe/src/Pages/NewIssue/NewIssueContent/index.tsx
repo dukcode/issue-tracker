e625a387537b873bd50cb8dfce6d8542f68eb4cf
JinJeon <nanyong01@gmail.com>
2022-06-24 14:47:07 +0900

    feat: image file upload 기능 일부 추가

--- fe/src/Pages/NewIssue/NewIssueContent/index.tsx
+++ fe/src/Pages/NewIssue/NewIssueContent/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import UserImg from "Component/UserImg";
 import user from "Img/user.jpeg";
@@ -8,6 +8,7 @@ import {
 	StyledNewIssueContent,
 	StyledNewIssueTitle,
 	StyledNewIssueDesc,
+	StyledUploadImg,
 } from "./NewIssueContent.styled";
 
 const defaultIssueTitle = "제목";
@@ -16,22 +17,42 @@ const ADD_FILE = "파일 첨부하기";
 const { FilePresent } = icons;
 
 const NewIssueContent = () => {
-	const [issueTitle] = useState(defaultIssueTitle);
-	const [issueDesc] = useState(defaultIssueDesc);
+	const [issueTitle, setIssueTitle] = useState("");
+	const [issueDesc, setIssueDesc] = useState("");
+	const [fileName, setFileName] = useState(ADD_FILE);
+
+	const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		setIssueTitle(value);
+	};
+
+	const handleDescChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const { value } = event.target;
+		setIssueDesc(value);
+	};
+
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { files } = event.target;
+		if (files) {
+			const { name } = files[0];
+			setFileName(name);
+		}
+	};
 
 	return (
 		<StyledNewIssueContentWrapper>
 			<UserImg img={user} size="medium" />
 			<StyledNewIssueContent>
 				<StyledNewIssueTitle>
-					<input value={issueTitle} />
+					<input value={issueTitle} onChange={handleTitleChange} placeholder={defaultIssueTitle} />
 				</StyledNewIssueTitle>
 				<StyledNewIssueDesc>
-					<textarea value={issueDesc} />
-					<div>
+					<textarea value={issueDesc} onChange={handleDescChange} placeholder={defaultIssueDesc} />
+					<input type="file" id="upload_img" onChange={handleFileChange} />
+					<StyledUploadImg htmlFor="upload_img">
 						<FilePresent colorset="label" size={16} />
-						{ADD_FILE}
-					</div>
+						{fileName}
+					</StyledUploadImg>
 				</StyledNewIssueDesc>
 			</StyledNewIssueContent>
 		</StyledNewIssueContentWrapper>
