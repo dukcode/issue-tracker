49182701c47b103a79f6825e71d8506e6a0dd305
JinJeon <nanyong01@gmail.com>
2022-06-29 13:04:20 +0900

    refactor: token api를 react query로 처리하도록 변경

--- fe/src/Api/index.ts
+++ fe/src/Api/index.ts
@@ -1,7 +1,6 @@
-import tokenApi from "./tokenApi";
 import usersApi from "./usersApi";
 import milestoneApi from "./milestoneApi";
 import issuesApi from "./issuesApi";
 import labelsApi from "./labelsApi";
 
-export { tokenApi, usersApi, milestoneApi, issuesApi, labelsApi };
+export { usersApi, milestoneApi, issuesApi, labelsApi };
